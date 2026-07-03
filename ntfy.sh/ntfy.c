256e9572ce7ecc0aa81fa94355f6427804b6ba86
IamShivanshCoder <shivanshbansal0308@gmail.com>
2026-07-03 20:07:21 +0530

    added notification project basic using ntfy.sh and libcurl

---
+++ ntfy.sh/ntfy.c
@@ -0,0 +1,22 @@
+#include <curl/curl.h>
+#include <curl/easy.h>
+#include <curl/typecheck-gcc.h>
+#include <stdio.h>
+
+int main(void) {
+  CURL* curl;
+  CURLcode res;
+  char* data = "hi,this is a notification";
+  curl = curl_easy_init();
+  if (curl) {
+    curl_easy_setopt(curl, CURLOPT_URL, "https://ntfy.sh/shivansh_alerts");
+    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data);
+    res = curl_easy_perform(curl);
+    if (res != CURLE_OK) {
+      fprintf(stderr, "curl_easy_perform() failed:  %s\n",
+              curl_easy_strerror(res));
+    }
+    curl_easy_cleanup(curl);
+  }
+  return 0;
+}
